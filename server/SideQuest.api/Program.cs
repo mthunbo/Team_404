d9c8353a4e59e631294db914e50f4dea03f9a2c0
mthunbo <mthunbov2@gmail.com>
2026-01-22 10:29:33 +0100

    Update dev to fit main (#16)
    * Removed initial project example for clean start (#2)
    
    * Chore/project setup (#15)
    
    * Removed initial project example for clean start
    
    * Initial setup of dotnet backend

---
+++ server/SideQuest.api/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Controllers
+builder.Services.AddControllers();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MobileDev", policy =>
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("MobileDev");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("/", () => Results.Ok("API is running"));
+
+app.Run();
