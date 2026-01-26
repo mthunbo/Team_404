a84c94f9f2996d311eca01d4807c13958fff2ecb
CueBaZe <olivervalente@icloud.com>
2026-01-26 11:58:56 +0100

    Task container made

--- client/SideQuest/app/(tabs)/dashboard.tsx
+++ client/SideQuest/app/(tabs)/dashboard.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import { useRouter } from 'expo-router';
 
 export default function Dashboard() {
     return (
-        <View className='flex-1 justify-center items-center'>
-            <Text className='text-xl text-red-500 font-bold text-center'>This is the Dashboard</Text>
+        <View className='flex-1 bg-[#B3B3B3] justify-center items-center'>
+            <View className='w-[300px] h-[600px]'>
+                <ScrollView className='bg-[#D9D9D9] rounded-lg border border-[5px] border-[#A5A5A5] p-4'>
+                    {/* Tasks will be loaded here */}
+                </ScrollView>
+            </View>
         </View>
     );
 }
