fedc755fed4dd2e0c44901a51787faf406917e6f
CueBaZe <olivervalente@icloud.com>
2026-01-27 14:25:10 +0100

    Added the settings button

--- client/SideQuest/app/(tabs)/dashboard.tsx
+++ client/SideQuest/app/(tabs)/dashboard.tsx
@@ -8,18 +8,26 @@ export default function Dashboard() {
     return (
         <View className='flex-1 flex-col bg-[#B3B3B3] justify-center items-center gap-[20px]'>
 
+            <View className='flex flex-row justify-between px-6'>
+                {/* Settings button */}
+                <View>
+                    <TouchableOpacity>
+                        <Ionicons name='settings-sharp' color={'#fff'} size={40} />
+                    </TouchableOpacity>
+                </View>
 
-            {/* Parrent view */}
-            <View className='flex flex-row self-end mr-[20px]'>
-                <TouchableOpacity onPress={() => alert('Button pressed!')}><Ionicons name='add' color={'#fff'} size={40}></Ionicons></TouchableOpacity>
-            </View>
+                {/* Parrent view */}
+                <View className='flex flex-row self-end mr-[20px]'>
+                    <TouchableOpacity onPress={() => alert('Button pressed!')}><Ionicons name='add' color={'#fff'} size={40}></Ionicons></TouchableOpacity>
+                </View>
 
-            {/*
-            <View className='flex flex-row self-end mr-4 border border-[4px] border-[#797979] rounded-2xl px-[10px] py-[4px]'> // Coin display (ONLY CHILDREN VIEW)
-                <Text className='text-3xl -mt-[1px] mr-2 text-white font-bold'>150</Text>
-                <Image source={require('../assets/Coin.png')} style={{ width: 30, height: 30 }} resizeMode="contain" />
+                {/*
+                <View className='flex flex-row self-end mr-4 border border-[4px] border-[#797979] rounded-2xl px-[10px] py-[4px]'> // Coin display (ONLY CHILDREN VIEW)
+                    <Text className='text-3xl -mt-[1px] mr-2 text-white font-bold'>150</Text>
+                    <Image source={require('../assets/Coin.png')} style={{ width: 30, height: 30 }} resizeMode="contain" />
+                </View>
+                */}
             </View>
-            */}
 
                 <Text className='text-orange-600 text-4xl font-bold'>Quests</Text>
 
