8e14b976438f6c5f8b8c1bfa727392ff850ed641
mthunbo <mthunbov2@gmail.com>
2026-01-27 13:06:33 +0100

    Feature/namespace (#50)
    * feat(quests): Added models for quest + template for interface and service.
    
    * feat(quests): Made models for parent and child-user, and a common base class for the users.
    
    * feat(quests): Finished making the models for the database.
    
    * feat(models): Adjusted the models to fit the updates in our table.
    
    * feat(models): Adjusted the last tables again and fixed some nullable issues
    
    * feat(namespace) Corrected the models to have the proper namespace attached to it.
    
    ---------
    
    Co-authored-by: Lysell92 <157490830+Lysell92@users.noreply.github.com>

--- server/SideQuest.api/Models/Reward.cs
+++ server/SideQuest.api/Models/Reward.cs
@@ -1,11 +1,8 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
-<<<<<<< HEAD
-=======
 namespace SideQuest.api.Models;
 
->>>>>>> feature/namespace
 // The rewards a child-user can pick between
 public class Reward
 {
